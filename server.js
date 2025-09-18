6ca5597e5348236d6099964e31718d6c34c39fb6
taloe1524 <ta15.ch24.leaner@gmail.com>
2025-09-18 12:49:15 -0700

    bai23

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-const { createServer } = require("node:http"); //no need to install "require"
-// boi vi no duoc tich hop trong nodejs roi,chi can goi ra va su dung
+const express = require("express"); //common js
+const path = require("path");
+require("dotenv").config();
 
-const hostname = "localhost"; //localhost === 127.0.0.1  - range 127.0.0.1 -> 127.0.0.255
-const port = 7000; // tham so port range 1 -> around 65000. advoid 21 (file transfer protocol)
-// 23 (telnet), 80 (HTTP), 443 (HTTPS). frontend port use 3000 or 3001 3002, backend 8000,8001,8002
+console.log("check env:", process.env);
+// import express from "express"; //es module
+const app = express();
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+//config template engine ejs
+// app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("view engine", "pug");
+
+//config static files
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.send("Hello World port 8080 da them nodemon and test nodemon!");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/abc", (req, res) => {
+  // res.send("<h1>day la doan text da duoc route</h1>");
+  res.render("sample.ejs");
+});
+
+app.get("/pp", (req, res) => {
+  res.render("index", { title: "Hey", message: "Hello there!" });
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Example app listening on port ${port}`);
 });
